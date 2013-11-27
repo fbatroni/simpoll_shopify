3dec6f75b4f1c1af41ba494f99b3603a6ffffefa
Nana Ewusi <precious.ewusi.nyarko@meltwater.org>
2013-11-27 13:24:57 +0000

    Added products model

---
+++ model/product.js
@@ -0,0 +1,29 @@
+/*
+* Module Dependencies
+*/
+var mongoose = require('mongoose'),
+	Product = mongoose.model('Product');
+
+function save(products, callback) {
+	products = (products instanceof Array) ? products : [products];
+	Product.create(products, function (err) {
+		if (err) callback(err);
+		else {
+			var saved = Array.prototype.slice.call(arguments, 1);
+			callback(null, saved);
+		}
+	});
+}
+
+function byName(name, callback) {
+	Product.find()
+	.where('name')
+	.equals(name)
+	.exec(function (err, products) {
+		if (err) callback(err);
+		callback(null, products[0]);
+	});
+}
+
+exports.save = save;
+exports.findByName = byName;
